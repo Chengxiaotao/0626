b776d68972760efb3828ad581d268a4f59fc5321
chengxiaotao <41604130@xs.ustb.edu.cn>
2020-08-07 17:15:09 +0800

    20/0/0 周_171509

--- autoGit.bat
+++ autoGit.bat
@@ -1,7 +1,7 @@
 cd F:\Extracurricular\Github-infos\Pic
 git add .
 echo "add finished"
-git commit -m %date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
 echo "commit finished"
 git push origin master
 echo "push finished"
